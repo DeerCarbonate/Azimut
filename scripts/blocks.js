0d3d178b09405d3f256b1b89995e61b4fbcb8613
DeerCarbonate <rus61337@gmail.com>
2025-05-10 00:16:29 +0600

    у

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,7 +1,11 @@
 // Логистика. Logistic
-// Взят пример из asteriom
+// Взят пример из asterion
 //пример: const "кодовое название предмета" = extend("Класс/type", "в кавычках название блока json", {});
 
 const ducts = extend(Duct, "ducts", {});
 
 const ductsjunction = extend(Junction, "ducts-junction", {});
+
+const bridgeducts = extend(DuctBridge, "bridge-ducts", {});
+
+const ductsrouter = extend(Router, "ducts-router", {});
