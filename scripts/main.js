11c3aaaa2da7c914a0c0bff6840b76c02316c0a0
KOSTOLOM <76535433+KOCTOLOM@users.noreply.github.com>
2025-06-23 02:55:45 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+importClass(java.lang.Class);
+
+const mod = Vars.mods.locateMod("azi");
+
+mod.meta.displayName = "[#b867ec]A[#c974dc]Z[#bb9ef3ff]I[#9a5bf6]M[#934bee]U[#8b75e1]";
+mod.meta.description = "KO[#A4A4A4]STO[#6D6D6D]LOM [white]";
+mod.meta.author = "DeerCarbonate";
+
+require("blocks");
