439696eb89e4e5598c837e6284fe1720980ad850
KOSTOLOM <76535433+KOCTOLOM@users.noreply.github.com>
2025-06-23 03:06:18 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 importClass(java.lang.Class);
 
-const mod = Vars.mods.locateMod("azi");
+const mod = Vars.mods.locateMod("azimut");
 
 mod.meta.displayName = "[#b867ec]A[#c974dc]Z[#bb9ef3ff]I[#9a5bf6]M[#934bee]U[#8b75e1]T";
 mod.meta.description = "[#c5faf6]Develop complex logistics[],[#c5fae3] repel waves[],[#fac5d3]destroy enemy bases[] on the [#bbc4f4]NEW planet[] [#768bff]Gelion[].\n\n\t[red]THE MOD IS CURRENTLY UNDER DEVELOPMENT[], there are already 4 sectors at the moment, and work is underway on quality of life.\n\n\tSpecials thanks to: KO[#A4A4A4]STO[#6D6D6D]LOM [white], ace1020spawn, Kivet/БОБ, Alpotat";
