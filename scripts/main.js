3ab08e227c6c3e41bdbf50db3d14ba199df5bb93
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-02-11 07:12:50 +0500

    0.33 update

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,31 @@ importClass(java.lang.Class);
 
 const mod = Vars.mods.locateMod("azimut");
 
-mod.meta.displayName = "[#b867ec]A[#c974dc]Z[#bb9ef3ff]I[#9a5bf6]M[#934bee]U[#8b75e1]T";
-mod.meta.description = "[#c5faf6]Develop complex logistics[],[#c5fae3] repel waves[],[#fac5d3]destroy enemy bases[] on the [#bbc4f4]NEW planet[] [#768bff]Gelion[].\n\n\t[red]THE MOD IS CURRENTLY UNDER DEVELOPMENT[], there are already 9 sectors at the moment, and work is underway on quality of life.\n\n\tSpecials thanks to: KO[#A4A4A4]STO[#6D6D6D]LOM [white], ace1020spawn, Kivet/БОБ, Alpotat, Fate of Adrastus";
+mod.meta.displayName =
+"[#b867ec]A[#c974dc]Z[#bb9ef3ff]I[#9a5bf6]M[#934bee]U[#8b75e1]T";
+
+mod.meta.description =
+"A [#c5faf6]hybrid mod[] primarily built around [#bbc4f4]Erekir[] and its linear progression system.\n" +
+"The mod introduces new gameplay mechanics that are tightly integrated into the campaign and accompany you throughout the entire playthrough.\n\n" +
+
+"The mod also has a [#768bff]Discord server[] — you can find the link in the README file on the GitHub page.\n\n" +
+
+"[red]THIS MOD IS STILL IN DEVELOPMENT![]\n" +
+"At the moment, development is approximately [accent]47%[] complete and features [accent]9 fully polished sectors[].\n\n" +
+
+"////////////////////////////////\n\n" +
+
+"Land on a [#c9c1eb]subcryogenic terra[], gradually warming under its star, known as [#768bff]Gelion[].\n" +
+"Your objective is to eliminate enemy forces on the planet, secure rare resources, and continue your expansion across the galaxy.\n\n" +
+
+"Limited space and complex logistics present a serious challenge.\n" +
+"[#fac5d3]Plan your production wisely![]\n\n" +
+
+"If you enjoy the mod, consider giving it a star! >~<\n" +
+"Thanks for playing!\n\n" +
+
+"Special thanks to: ace1020spawn, Kivet/БОБ, Alpotat, KO[#A4A4A4]STO[#6D6D6D]LOM [white], Fate of Adrastus";
+
 mod.meta.author = "[#c3961f]Deer[#876409]Carbonate";
-  
+
 require("blocks");
-
-
