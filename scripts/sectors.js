bd08b8ff32d3de6f1755dd258a438da34a995c91
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-02-24 09:18:33 +0500

    0.34 ųpđäţę

---
+++ scripts/sectors.js
@@ -0,0 +1,11 @@
+// Сектора кампании Azimut
+
+const first = extend(SectorPreset, "first", {});
+const twos = extend(SectorPreset, "twos", {});
+const threes = extend(SectorPreset, "threes", {});
+const four = extend(SectorPreset, "four", {});
+const sixth = extend(SectorPreset, "sixth", {});
+const fifth = extend(SectorPreset, "fifth", {});
+const seven = extend(SectorPreset, "seven", {});
+const eight = extend(SectorPreset, "eight", {});
+const nine = extend(SectorPreset, "nine", {});
