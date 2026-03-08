34262b96a6655a2970a88308f8f8c53678bf10a3
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-03-08 10:57:50 +0500

    0.4 update

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ importClass(java.lang.Class);
 const mod = Vars.mods.locateMod("azimut");
 
 mod.meta.displayName =
-"[#b867ec]A[#c974dc]Z[#bb9ef3ff]I[#9a5bf6]M[#934bee]U[#8b75e1]T";
+"[#b867ec]A[#c974dc]Z[#bb9ef3ff]I[#9a5bf6]M[#934bee]U[#8b75e1]T[#c1abd4]H";
 
 mod.meta.description =
 "A [#c5faf6]hybrid mod[] primarily built around [#bbc4f4]Erekir[] and its linear progression system.\n" +
