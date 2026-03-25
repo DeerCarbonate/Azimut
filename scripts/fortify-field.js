8b4bb01ad59245b4c9f03ee0e5ca01d401adf94a
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-03-25 12:29:58 +0500

    0.41 update

--- scripts/fortify-field.js
+++ scripts/fortify-field.js
@@ -2,26 +2,42 @@ try {
     Log.info("fortify-field 1");
     importClass(Packages.mindustry.ctype.ContentType);
     importClass(Packages.arc.util.Time);
-    Log.info("fortify-field 2");
 
-    const fortified = Vars.content.getByName(ContentType.status, "azimut-fortified");
+    Log.info("fortify-field 2");
+    const statuses = Vars.content.statusEffects();
+    Log.info("statuses count: " + statuses.size);
+
+    let fortified = null;
+    for(let i = 0; i < statuses.size; i++){
+        const s = statuses.get(i);
+        if(s.name === "azimut-fortified"){
+            fortified = s;
+            break;
+        }
+    }
     Log.info("fortified: " + fortified);
 
-    const target = Vars.content.getByName(ContentType.unit, "azimut-fortification-drone");
+    const units = Vars.content.units();
+    let target = null;
+    for(let i = 0; i < units.size; i++){
+        const u = units.get(i);
+        if(u.name === "azimut-fortification-drone"){
+            target = u;
+            break;
+        }
+    }
     Log.info("target: " + target);
 
-    if (fortified && target) {
+    if(fortified && target){
         const FortifyFieldAbility = JavaAdapter(
             Packages.mindustry.entities.abilities.UnitAbility, {
-            range: 83,
-            reload: 10,
             _timer: 0,
             update(unit) {
                 this._timer += Time.delta;
-                if (this._timer < this.reload) return;
+                if(this._timer < 10) return;
                 this._timer = 0;
                 Groups.unit.each(ally => {
-                    if (ally.team === unit.team && ally.dst(unit.x, unit.y) <= this.range) {
+                    if(ally.team === unit.team && ally.dst(unit.x, unit.y) <= 83){
                         ally.apply(fortified, 120);
                     }
                 });
