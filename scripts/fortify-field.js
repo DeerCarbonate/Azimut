f66454daf1a1aa9b4f8868b83faaba1d09ee337a
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-03-25 12:12:16 +0500

    0.41 update

--- scripts/fortify-field.js
+++ scripts/fortify-field.js
@@ -1,35 +1,40 @@
-// fortify-field.js
-const ContentType = Java.type("mindustry.ctype.ContentType");
-const Time = Java.type("arc.util.Time");
-const UnitAbility = Java.type("mindustry.entities.abilities.UnitAbility");
+try {
+    Log.info("fortify-field script started");
 
-const fortified = Vars.content.getByName(ContentType.status, "azimut-fortified");
-Log.info("fortified status: " + fortified);
+    const ContentType = Java.type("mindustry.ctype.ContentType");
+    const Time = Java.type("arc.util.Time");
+    const UnitAbility = Java.type("mindustry.entities.abilities.UnitAbility");
 
-const target = UnitTypes.find("azimut-fortification-drone");
-Log.info("target unit: " + target);
+    Log.info("classes loaded");
 
-if (fortified && target) {
-    const FortifyFieldAbility = JavaAdapter(UnitAbility, {
-        range: 83,
-        reload: 10,
-        _timer: 0,
+    const fortified = Vars.content.getByName(ContentType.status, "azimut-fortified");
+    Log.info("fortified: " + fortified);
 
-        update(unit) {
-            this._timer += Time.delta;
-            if (this._timer < this.reload) return;
-            this._timer = 0;
+    const target = UnitTypes.find("azimut-fortification-drone");
+    Log.info("target: " + target);
 
-            Groups.unit.each(ally => {
-                if (ally.team === unit.team && ally.dst(unit.x, unit.y) <= this.range) {
-                    ally.apply(fortified, 120);
-                }
-            });
-        },
-
-        copy() { return this; }
-    });
-
-    target.abilities.add(new FortifyFieldAbility());
-    Log.info("FortifyField added!");
+    if (fortified && target) {
+        const FortifyFieldAbility = JavaAdapter(UnitAbility, {
+            range: 83,
+            reload: 10,
+            _timer: 0,
+            update(unit) {
+                this._timer += Time.delta;
+                if (this._timer < this.reload) return;
+                this._timer = 0;
+                Groups.unit.each(ally => {
+                    if (ally.team === unit.team && ally.dst(unit.x, unit.y) <= this.range) {
+                        ally.apply(fortified, 120);
+                    }
+                });
+            },
+            copy() { return this; }
+        });
+        target.abilities.add(new FortifyFieldAbility());
+        Log.info("FortifyField added!");
+    } else {
+        Log.warn("fortified=" + fortified + " target=" + target);
+    }
+} catch(e) {
+    Log.err("fortify-field error: " + e);
 }
