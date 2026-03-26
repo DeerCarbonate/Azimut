29fcb37565540ac19a64fa691ccdf6b669fb7127
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-03-26 09:01:22 +0500

    0.41 YAYU VIBECODING (I STARTING HATE MYSELF)

--- scripts/fortify-field.js
+++ scripts/fortify-field.js
@@ -1,30 +1,33 @@
 try {
     Log.info("fortify-field 1");
     importClass(Packages.arc.util.Time);
+    importClass(Packages.mindustry.entities.abilities.EnergyFieldAbility);
     Log.info("fortify-field 2");
 
-    const FortifyFieldAbility = extend(Packages.mindustry.entities.abilities.UnitAbility, {
-        _timer: 0,
+    const FortifyFieldAbility = extend(EnergyFieldAbility, {
         update(unit) {
-            this._timer += Time.delta;
-            if(this._timer < 10) return;
-            this._timer = 0;
+            this.super$update(unit);
             Groups.unit.each(ally => {
                 if(ally.team === unit.team && ally.dst(unit.x, unit.y) <= 83){
                     const eff = Vars.content.statusEffects().find(s => s.name === "azimut-fortified");
                     if(eff) ally.apply(eff, 120);
                 }
             });
-        },
-        copy() { return this; }
+        }
     });
 
     Log.info("fortify-field 3");
+    const inst = new FortifyFieldAbility();
+    inst.range = 83;
+    inst.reload = 10;
+    inst.damage = 20;
+    inst.maxTargets = 7;
+    inst.healPercent = 0.1;
 
     const units = Vars.content.units();
     for(let i = 0; i < units.size; i++){
         if(units.get(i).name === "azimut-fortification-drone"){
-            units.get(i).abilities.add(new FortifyFieldAbility());
+            units.get(i).abilities.add(inst);
             Log.info("FortifyField added!");
             break;
         }
