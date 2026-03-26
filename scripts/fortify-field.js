feeb8ba39f8846de151342abb764d0f82f583957
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-03-26 09:06:57 +0500

    0.41 update

--- scripts/fortify-field.js
+++ scripts/fortify-field.js
@@ -1,37 +1,30 @@
 try {
     Log.info("fortify-field 1");
-    importClass(Packages.arc.util.Time);
-    importClass(Packages.mindustry.entities.abilities.EnergyFieldAbility);
+
+    const UnitUpdateEvent = Packages.mindustry.game.EventType.UnitUpdateEvent;
     Log.info("fortify-field 2");
 
-    const FortifyFieldAbility = extend(EnergyFieldAbility, {
-        update(unit) {
-            this.super$update(unit);
+    let fortified = null;
+
+    Events.on(UnitUpdateEvent, e => {
+        const unit = e.unit;
+        if(unit.type.name !== "azimut-fortification-drone") return;
+
+        if(!fortified){
+            fortified = Vars.content.statusEffects().find(s => s.name === "azimut-fortified");
+            if(!fortified) return;
+        }
+
+        if(unit.timer.get(5, 10)){
             Groups.unit.each(ally => {
                 if(ally.team === unit.team && ally.dst(unit.x, unit.y) <= 83){
-                    const eff = Vars.content.statusEffects().find(s => s.name === "azimut-fortified");
-                    if(eff) ally.apply(eff, 120);
+                    ally.apply(fortified, 120);
                 }
             });
         }
     });
 
-    Log.info("fortify-field 3");
-    const inst = new FortifyFieldAbility();
-    inst.range = 83;
-    inst.reload = 10;
-    inst.damage = 20;
-    inst.maxTargets = 7;
-    inst.healPercent = 0.1;
-
-    const units = Vars.content.units();
-    for(let i = 0; i < units.size; i++){
-        if(units.get(i).name === "azimut-fortification-drone"){
-            units.get(i).abilities.add(inst);
-            Log.info("FortifyField added!");
-            break;
-        }
-    }
+    Log.info("fortify-field ready!");
 } catch(e) {
     Log.err("fortify-field error: " + e);
 }
